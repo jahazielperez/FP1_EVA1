095a48f9d19b20c181ca442f4ade13684bafb9fd
jahazielperez <l26550312@chihuahua2.TECNM.MX>
2026-09-03 19:50:16 -0500

    Add files via upload

---
+++ EVA1/EVA1_2_HOLA MUNDO/src/eva1_2_hola/mundo/EVA1_2_HOLAMUNDO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_hola.mundo;
+
+/**
+ *
+ * @author jahaziel
+ */
+public class EVA1_2_HOLAMUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] arge) {//INICIO DEL PROGRAMA
+            //ESTO ES UN BLOQUE DE CODIGO
+            //MOSTRAR UN MENSAJE DE TEXTO
+            //MOSTRAR ---> INSTRUCCION
+            //TEXTO ---> DATOS
+            System.out.println("Hola mundo");
+          
+            
+    
+                    }
+                    }
