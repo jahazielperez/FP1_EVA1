7c0c52143e0b9669f7aa3a3e7757f378269d8a7f
jahazielperez <l26550312@chihuahua2.TECNM.MX>
2026-09-03 19:46:45 -0500

    Add files via upload

---
+++ EVA1_3_SALIDA/EVA1_3_SALIDAS/src/eva1_3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+/**
+ *
+ * @author jahaziel
+ */
+public class EVA1_3_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+           System.out.println("bienvenido a fundamientos de programacion.");
+           System.out.println("jahaziel perez juarez.");
+           System.out.print("tu calificacion sera.");
+           System.out.println(85);
+           
+    }
+    
+}
