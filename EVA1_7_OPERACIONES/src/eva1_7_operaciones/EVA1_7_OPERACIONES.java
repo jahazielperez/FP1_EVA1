26790cc628031fa503c846b198b0fac299fa7ae8
jahazielperez <l26550312@chihuahua2.TECNM.MX>
2026-09-14 22:54:32 -0500

    Add files via upload

---
+++ EVA1_7_OPERACIONES/src/eva1_7_operaciones/EVA1_7_OPERACIONES.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_operaciones;
+
+/**
+ *
+ * @author jahaziel
+ */
+public class EVA1_7_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     //el tipo de datos determine las operaciones 
+     //enteros, reales, cadenas, logicos, etc
+     //EXPRESION:
+     //VALORES 
+     //VARIABLES
+     //      OPERADORES 
+     //FUNCIONES
+     //QUE SE EVALUAN PARA OBTENER UN RESULTADO
+     //OPERACIONES CON NUMEROS:
+     // ARITMETICAS
+     int suma, val1, val2;//DECLARAMOS 3 VARIABLES
+     //ASIGNACION: PONER VALOR A UNA VARIABLE
+     //OPERADOR --> SIMBOLO QUE TRABAJA SOBRE IDENTIFICADORES 
+     //*---> ASIGNACION --> PONER EL VALOR
+     //** -->COMPARACION
+     
+     
+     val1 = 100; //asignar el valor de 100 a la variable val1
+     val2 = 50;
+     suma = val1 + val2;//EXPRESION, ASIGNAR A LA VARIABLE SUMA DE LA OPERACION ENTRE
+     //+ --> ESTA ENTRE 2 ENTEROS SE INTERPRETA COMO LO QUE DEBE SUMAR 
+     System.out.print("valor 1 =");
+     System.out.println(val1);
+     System.out.print("valor 2 =");
+     System.out.println(val2);
+     System.out.print("suma =");
+     System.out.println(suma);
+     //operaciones respetan reglas matematicas
+     //MULTIPLICACION
+     //*
+     int mult;
+     mult = val1 * val2;
+     System .out.print("multiplicacion =");
+     System.out.println(mult);
+     //division 
+     // / -----> OPERADOR DE DIVISION 
+     int div;
+     div = val1 / val2;
+     System.out.print("division =");
+     System.out.println(div);
+     val1 = 10;
+     val2 = 3;
+     System.out.print("valor 1 =");
+     System.out.println(val1);
+     System.out.print("valor 2 =");
+     System.out.println(val2);
+     div = val1 / val2;
+     System.out.print("division =");
+     System.out.println(div);
+     double resu;
+     resu = val1 / val2;
+     System.out.print("resu =");
+     System.out.println(resu);
+     double val2cop;
+     val2cop = 3;
+     resu = val1 / val2cop;
+     System.out.print("resultado =");
+     System.out.println(resu);
+     
+     
+             
+    }
+    
+}
