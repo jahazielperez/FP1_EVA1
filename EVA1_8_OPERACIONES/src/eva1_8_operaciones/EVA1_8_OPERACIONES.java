fce72d69efba046a2444939b13b53de22e038642
jahazielperez <l26550312@chihuahua2.TECNM.MX>
2026-09-14 22:54:51 -0500

    Add files via upload

---
+++ EVA1_8_OPERACIONES/src/eva1_8_operaciones/EVA1_8_OPERACIONES.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_operaciones;
+
+/**
+ *
+ * @author jahaziel
+ */
+public class EVA1_8_OPERACIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int resu, val1 = 10, val2 = 5;//declarando e INICIALIZANDO
+        //JAVA ES BUENA ONDA: INICIALIZA EN AUTOMATICO LAS VARIABLES.
+        //NUMERO ES CERO, BOOLEANOS EN FALSO
+        //niveles de operadores.
+        resu = 100 + val1 * (val1/val2);
+        System.out.print("resu =");
+        System.out.println(resu);
+        
+    }
+    
+}
