ef0481a5a2d9eba395c9a63a62bf0a7ee3932ffe
jahazielperez <l26550312@chihuahua2.TECNM.MX>
2026-09-14 22:55:27 -0500

    Add files via upload

---
+++ EVA1_9_OPERADORES/src/eva1_9_operadores/EVA1_9_OPERADORES.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_9_operadores;
+
+/**
+ *
+ * @author jahaziel
+ */
+public class EVA1_9_OPERADORES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //TAMBIEN HAY OPERACIONES PARA CADENAS 
+        //CONCATENACION: PEGAR CADENAS DE TEXTO --> +
+        String cade;
+        cade = "hola"  + "  "+"mundo";
+        System.out.println(cade);
+        cade = "valor = " + 100;
+        System.out.println(cade);
+        cade = "Valores booleanos =" + false;
+        System.out.print(cade);
+        
+        
+        
+    }
+    
+}
